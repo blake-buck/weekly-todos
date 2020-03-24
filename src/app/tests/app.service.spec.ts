1d1c196ec2af87e2c3a6c3b1b2bd0bc5eee5259e
blake-buck <blakemanbuck@gmail.com>
2020-03-24 11:31:32 -0500

    wrote tests for appService

--- src/app/tests/app.service.spec.ts
+++ src/app/tests/app.service.spec.ts
@@ -1,19 +1,36 @@
 import { AppService } from "../store/app.service";
 import { TestBed } from '@angular/core/testing';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
-import { initialState } from '../store/app.state';
+import { initialState, AppStore } from '../store/app.state';
 
-describe('AppService', async () => {
+describe('AppService', () => {
     let service: AppService;
-    let store = TestBed.inject(MockStore)
-    console.log('STORE ', store)
+    let store: MockStore<AppStore>
+
     beforeEach(() => {
         TestBed.configureTestingModule({
             providers:[
-                provideMockStore({initialState})
+                provideMockStore({initialState:{app:initialState}})
             ]
         })
+
+        store = TestBed.inject(MockStore)
     })
     beforeEach(() => service = new AppService(store));
+    beforeEach(() => localStorage.clear());
+
+    it('storeState should store state in local storage', () => {
+        service.storeState()
+        expect(localStorage.getItem('state')).toBeTruthy()
+    })
+
+    it('getStateFromStorage should get state from local storage', () => {
+        localStorage.setItem('state', JSON.stringify({...initialState, selectedWeek:'1-2000'}))
+
+        let state = service.getStateFromStorage();
+
+        expect(state).toBeTruthy();
+        expect(state.selectedWeek).toBe('1-2000')
+    })
 
 })
